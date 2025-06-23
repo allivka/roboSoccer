46c61cddd81fb09d2c909d6542b82de19e067774
allivka <allivka1920@gmail.com>
2025-06-23 17:37:01 +0300

    added every direction movement (no tests yet)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 
-#define RE 5
-#define RM 4
+#define LE 5
+#define LM 4
+
+#define RE 10
+#define RM 11
 
 #define BE 6
 #define BM 7
@@ -11,31 +14,83 @@ public:
     int speedPin;
     int dirPin;
     
-    Motor(int p_speedPin, int p_dirPin) : speedPin(p_speedPin), dirPin(p_dirPin) {
+    Motor() = default;
+    
+    Motor(int p_speedPin, int p_dirPin) : speedPin(p_speedPin), dirPin(p_dirPin) {}
+    
+    void initPins() {
         pinMode(speedPin, OUTPUT);
         pinMode(dirPin, OUTPUT);
     }
     
     void run(int speed) {
         
-        speed %= 101;
-        
-        if(speed < 0) {
+        if(speed >= 0) {
             digitalWrite(dirPin, 0);
             speed = -speed;
         } else {
             digitalWrite(dirPin, 1);
         }
         
+        speed %= 101;
+                
         analogWrite(speedPin, abs(speed * 2.55));
     }
 };
 
-Motor R(RE, RM), B(BE, BM);
+class RobotSpeeds {
+public:
+    int L = 0;
+    int R = 0;
+    int B = 0;
+    RobotSpeeds(int l, int r, int b) : L(l), R(r), B(b) {}
+};
+
+RobotSpeeds countSpeeds(int alpha, int speed, int w) {
+    return RobotSpeeds(
+        speed * sin(-60 - alpha) + w,
+        speed * sin(60 - alpha) + w,
+        speed * sin(180 - alpha) + w
+    );
+}
+
+class Robot {
+public:
+    Motor L;
+    Motor R;
+    Motor B;
+    
+    Robot() = default;
+    
+    Robot(const Motor& l, const Motor& r, const Motor& b) : L(l), R(r), B(b)  {
+        L.initPins();
+        R.initPins();
+        B.initPins();
+    }
+    
+    void run(const RobotSpeeds& speeds) {
+        L.run(speeds.L);
+        R.run(speeds.R);
+        B.run(speeds.B);
+    }
+    
+    void stop() {
+        L.run(0);
+        R.run(0);
+        B.run(0);
+    }
+};
+
+Robot robot(
+    Motor(LE, LM),
+    Motor(RE, RM),
+    Motor(BE, BM)
+);
 
 void setup() {
+    
 }
 
 void loop() {
-    B.run(-200);
+    
 }
